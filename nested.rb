6424644832e4698e0d57a4c1c9d9ac00dde00fab
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 23:29:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        }, 
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby", 
+          :languages => ["LISP", "C"]
         }
      }
-  
-    
 end
 
 def changing_alan
@@ -103,10 +105,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
